e875fe2c87fd7593834bf97a46e510fa1b6f4605
EC2 Default User <ec2-user@ip-172-31-15-98.ec2.internal>
2021-12-23 10:45:31 +0000

    [Finish]検索機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
   devise_for :users
   resources :users, only: [:show,:index,:edit,:update]
+  resources :relationships, only: [:create,:destroy]
+  get 'search' => 'searches#search'
   get 'user/followers/:id' => 'users#followers', as: 'followers'
   get 'user/followings/:id' => 'users#followings', as: 'followings'
-  resources :relationships, only: [:create,:destroy]
-  
   resources :books, only: [:show,:index,:create,:edit,:update,:destroy]do
     resources :book_comments, only: [:create,:destroy]
     resource :favorites, only: [:create,:destroy]
